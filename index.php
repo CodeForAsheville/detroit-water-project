d1404b5d5ad08eb3aa3bdb6db9d559bc01d493f7
Patrick Conant <patrick@prcapps.com>
2014-10-05 23:52:17 -0400

    Initial Templating Work

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo file_get_contents("index.html");
+<?php 
+// PRC - Simple Templating
+
+$city_name = "Asheville";
+$city_state = "North Carolina";
+$city_name_full = "Asheville, NC";
+$site_url = 'ashevillewaterproject.org';
+$project_name = 'Asheville Water Project';
+
+echo file_get_contents("index.html");
